033303645efbf6f0bc11dffeb6f01945c3e6d3fa
Shivansh Mishra <mshivansh037@gmail.com>
2026-08-20 14:46:16 +0530

    Update Day 5 SQL aggregation

--- SQL/Day-05/aggregates.sql
+++ SQL/Day-05/aggregates.sql
@@ -1,12 +1,20 @@
 -- Day 5-SQL Aggregation Practice
+
+--COUNT()
+--AVG()
+--SUM()
+--GROUP BY()
+--HAVING
 --Leetcode 1075:Project Employees I
-# Write your MySQL query statement below
+
 SELECT project_id,
 ROUND(AVG(experience_years),2)
 AS average_years FROM Project 
 JOIN Employee ON Project.employee_id=Employee.employee_id 
 GROUP BY project_id;
+
 --LeetCode 570: Managers with at least 5 direct reports
+
 SELECT a.name FROM Employee a
 JOIN Employee b ON a.id = b.managerId
 GROUP BY b.managerId
